595814bac2e442fc3bb4329fa35530424041ee54
ChaithanyaPalvadi <chaithanya.palvadif5@gmail.com>
2026-03-06 12:44:42 +0530

    Update v1.1.1.sql

--- v1.1.1.sql
+++ v1.1.1.sql
@@ -1,7 +1,7 @@
 create or replace table EMP2(Name varchar)
 
 <<<<<<< Updated upstream
-insert into EMP2 values ('ball')git dc
-=======
 insert into EMP2 values ('ball')
+=======
+insert into EMP2 values ('ballS')
 >>>>>>> Stashed changes
